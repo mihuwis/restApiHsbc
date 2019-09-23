57d523d95450fd93fe68e081343e499344e44660
Michal Wisniewski <mihuwis@gmail.com>
2019-09-23 18:41:22 +0200

    First commit

---
+++ src/main/java/com/progrespoint/restapihsbc/RestapiHsbcApplication.java
@@ -0,0 +1,13 @@
+package com.progrespoint.restapihsbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiHsbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestapiHsbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/progrespoint/restapihsbc/RestapiHsbcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.progrespoint.restapihsbc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestapiHsbcApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
