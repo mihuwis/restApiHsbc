469a8ad726e18efb039c9eba0caae776b34ab27f
Michal Wisniewski <mihuwis@gmail.com>
2019-09-24 10:10:30 +0200

    Add implementation to Abstract Map Service. Add Base Entity class

--- src/main/java/com/progrespoint/restapihsbc/model/Customer.java
+++ src/main/java/com/progrespoint/restapihsbc/model/Customer.java
@@ -1,8 +1,30 @@
 package com.progrespoint.restapihsbc.model;
 
-public class Customer {
+public class Customer extends BaseEntity {
 
-    private long id;
+
     private String name;
     private Address address;
+
+    public Customer(Long id, String name, Address address) {
+        super(id);
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 }
--- src/main/java/com/progrespoint/restapihsbc/services/AbstractMapService.java
+++ src/main/java/com/progrespoint/restapihsbc/services/AbstractMapService.java
@@ -1,8 +1,53 @@
 package com.progrespoint.restapihsbc.services;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.progrespoint.restapihsbc.model.BaseEntity;
 
-public abstract class AbstractMapService<ID extends Long, T> {
-    Map<Long, T> map = new HashMap<>();
+import java.util.*;
+
+public abstract class AbstractMapService<ID extends Long, T extends BaseEntity> {
+
+    private Map<Long, T> map = new HashMap<>();
+
+    Set<T> findAll(){
+        return new HashSet<>(map.values());
+    }
+
+    T findByID(ID id){
+        return map.get(id);
+    }
+
+    T save(T object){
+        if(object != null){
+            setIdInObject(object);
+            map.put(object.getId(), object);
+        } else {
+            throw new NoSuchElementException("Object cannot be null");
+        }
+        return object;
+    }
+
+    void delete(T object){
+        map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    void deleteById(ID id){
+        map.remove(id);
+    }
+
+
+    private void setIdInObject(T object){
+        if(object.getId() == null){
+            object.setId(getNextIdNumber());
+        }
+    }
+
+    private Long getNextIdNumber(){
+        long id;
+        try {
+            id = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e){
+            id = 1L;
+        }
+        return id;
+    }
 }
