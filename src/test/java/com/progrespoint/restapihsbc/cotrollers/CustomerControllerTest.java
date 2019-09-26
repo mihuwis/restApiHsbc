e0d0462ff64646131af52d38b0df45e17409a2ea
Michal Wisniewski <mihuwis@gmail.com>
2019-09-26 13:45:56 +0200

    Implementing Controller test

--- src/test/java/com/progrespoint/restapihsbc/cotrollers/CustomerControllerTest.java
+++ src/test/java/com/progrespoint/restapihsbc/cotrollers/CustomerControllerTest.java
@@ -1,23 +1,32 @@
 package com.progrespoint.restapihsbc.cotrollers;
 
+import com.progrespoint.restapihsbc.services.CustomerService;
 import com.progrespoint.restapihsbc.services.map.AddressMapService;
 import com.progrespoint.restapihsbc.services.map.CustomerMapService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@WebMvcTest(CustomerController.class)
 class CustomerControllerTest {
 
     private MockMvc mockMvc;
+
+    @MockBean
+    private CustomerService customerService;
+
     private CustomerController customerController;
 
     private static String BASE_PATH = "http://localhost/customers";
 
     @BeforeEach
     void setUp() {
-        customerController = new CustomerController(new CustomerMapService(new AddressMapService()));
+       mockMvc = MockMvcBuilders.standaloneSetup(new CustomerController(customerService))
+               .build();
     }
 
     @Test
